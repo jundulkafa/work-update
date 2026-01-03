2234b5c1445d1dd9968505604e6902c5824cdd33
Jundul Kafa <jundulkafa@gmail.com>
2026-01-03 21:30:59 +0600

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -264,4 +264,8 @@ def logout():
 
 if __name__=="__main__":
     init_db()
-    app.run(debug=True)
+        app.run(debug=Tru
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
